0d51f78d0d14949a0a65d2a8f305cd1787033008
alefew <alefewyimer4@gmail.com>
2024-06-26 10:14:06 +0300

    reusable modal added using higher order compund function

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -6,18 +6,37 @@ import Modal from "../../ui/Modal";
 import CreateCabinForm from "./CreateCabinForm";
 
 export default function AddCabin() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
   return (
-    <div>
-      <Button onClick={() => setIsModalOpen((show) => !show)}>
-        Add new cabin
-      </Button>
-      {isModalOpen && (
-        <Modal onClose={() => setIsModalOpen(false)}>
-          <CreateCabinForm onCloseModal={() => setIsModalOpen(false)} />
-        </Modal>
-      )}
-    </div>
+    <Modal>
+      <Modal.Open opens="cabin-form">
+        <Button>Add new cabin</Button>
+      </Modal.Open>
+      <Modal.Window name="cabin-form">
+        <CreateCabinForm />
+      </Modal.Window>
+      {/* <Modal.Open opens="table">
+        <Button>Show table</Button>
+      </Modal.Open>
+      <Modal.Window name="table">
+        <CreateCabinForm />
+      </Modal.Window> */}
+    </Modal>
   );
 }
+
+// export default function AddCabin() {
+//   const [isModalOpen, setIsModalOpen] = useState(false);
+
+//   return (
+//     <div>
+//       <Button onClick={() => setIsModalOpen((show) => !show)}>
+//         Add new cabin
+//       </Button>
+//       {isModalOpen && (
+//         <Modal onClose={() => setIsModalOpen(false)}>
+//           <CreateCabinForm onCloseModal={() => setIsModalOpen(false)} />
+//         </Modal>
+//       )}
+//     </div>
+//   );
+// }
